7dad791e8213845126839afb4d6aaece4ba5eed3
michalisdikaiopoulos <michalisdikaiopoulos@gmail.com>
2026-02-28 18:05:50 +0100

    Debug App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster as SonnerToaster } from "sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -20,8 +19,7 @@ const RootRedirect = () => {
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
+      <SonnerToaster richColors />
       <BrowserRouter>
         <AuthProvider>
           <Routes>
